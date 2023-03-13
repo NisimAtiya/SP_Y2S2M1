04779ce175d0d1aa8747bb84173036054403fef6
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2023-03-13 20:07:12 +0200

    Update AdptArray.c

--- SP_Y2S2M1/ADT-main/AdptArray.c
+++ SP_Y2S2M1/ADT-main/AdptArray.c
@@ -30,9 +30,10 @@ PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df ,PRINT_FUNC pf){
 void DeleteAdptArray(PAdptArray p){
     if(p==NULL) return;
     for (int i = 0; i < p -> ArrSize; ++i) {
-        p -> del_func(p -> pElemArr[i]);
+        if(p -> pElemArr[i] != NULL)
+            p -> del_func(p -> pElemArr[i]);
     }
-    free(p->ArrSize);
+    free(p->pElemArr);
     free(p);
 }
 Result SetAdptArrayAt(PAdptArray p, int index, PElement pe){
@@ -43,21 +44,25 @@ Result SetAdptArrayAt(PAdptArray p, int index, PElement pe){
         memcpy(tempArr, p->pElemArr, (p->ArrSize) * sizeof(PElement));
         free(p->pElemArr);
         p->pElemArr = tempArr;
+        p->ArrSize = index + 1;
     }
-    p -> del_func((p -> pElemArr)[index]);
+    if ((p->pElemArr)[index] != NULL) {
+        p->del_func((p->pElemArr)[index]);
+    }
     (p->pElemArr)[index] = p -> copy_func(pe);
-    if(index>=p->ArrSize){
-        p -> ArrSize = index+1;
-    }
+
     return SUCCESS;
 }
 PElement GetAdptArrayAt(PAdptArray p, int index){
+    if(index<0) return NULL;
     if(p==NULL) return NULL;
-    if(p->ArrSize<index) return NULL;
+    if(p->ArrSize<=index) return NULL;
+    if((p->pElemArr)[index]==NULL) return NULL;
     return p->copy_func((p->pElemArr)[index]);
+
 }
 int GetAdptArraySize(PAdptArray p){
-    if(p==NULL)return -1;
+    if(p==NULL) return -1;
     if(p->pElemArr==NULL) return -1;
     return p->ArrSize;
 }
@@ -65,9 +70,25 @@ void PrintDB(PAdptArray p){
     if(p==NULL) printf("The PAdptArray you entered is null!");
     if(p->pElemArr==NULL) printf("The pElemArr of the PAdptArray you entered is null!");
     for (int i = 0; i < p->ArrSize; ++i) {
-        p->print_func((p->pElemArr)[i]);
+        if((p->pElemArr)[i] != NULL)
+            p->print_func((p->pElemArr)[i]);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
