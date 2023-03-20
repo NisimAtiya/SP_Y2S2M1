0dcaa3f4dabeec98da0252b1a959afc52d0ee2e7
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2023-03-20 18:54:52 +0200

    Update AdptArray.c

--- SP_Y2S2M1/ADT-main/AdptArray.c
+++ SP_Y2S2M1/ADT-main/AdptArray.c
@@ -28,7 +28,10 @@ PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df ,PRINT_FUNC pf){
 }
 
 void DeleteAdptArray(PAdptArray p){
-    if(p==NULL) return;
+    if(p==NULL) {
+        printf("The PAdptArray you entered is null!");
+        return;
+    }
     for (int i = 0; i < p -> ArrSize; ++i) {
         if(p -> pElemArr[i] != NULL)
             p -> del_func(p -> pElemArr[i]);
@@ -37,41 +40,61 @@ void DeleteAdptArray(PAdptArray p){
     free(p);
 }
 Result SetAdptArrayAt(PAdptArray p, int index, PElement pe){
-    if(p==NULL) return FAIL;
+    if(p==NULL) {
+        printf("The PAdptArray you entered is null!");
+        return FAIL;
+    }
     if(index >= p->ArrSize){
         PElement* tempArr = (PElement*)calloc((index+1) , sizeof(PElement));
-        if(tempArr == NULL) return FAIL;
+        if(tempArr == NULL) {
+            printf("Memory allocation failed");
+            return FAIL;
+        }
         memcpy(tempArr, p->pElemArr, (p->ArrSize) * sizeof(PElement));
         free(p->pElemArr);
         p->pElemArr = tempArr;
         p->ArrSize = index + 1;
     }
-    if ((p->pElemArr)[index] != NULL) {
-        p->del_func((p->pElemArr)[index]);
+    if (p->pElemArr[index] != NULL) {
+        p->del_func(p->pElemArr[index]);
     }
-    (p->pElemArr)[index] = p -> copy_func(pe);
+    p->pElemArr[index] = p -> copy_func(pe);
 
     return SUCCESS;
 }
 PElement GetAdptArrayAt(PAdptArray p, int index){
-    if(index<0) return NULL;
-    if(p==NULL) return NULL;
-    if(p->ArrSize<=index) return NULL;
-    if((p->pElemArr)[index]==NULL) return NULL;
-    return p->copy_func((p->pElemArr)[index]);
-
+    if(index<0) {
+        printf("The index you entered is negative");
+        return NULL;
+    }
+    if(p==NULL){
+        printf("The PAdptArray you entered is null!");
+        return NULL;
+    }
+    if(p->ArrSize<=index) {
+        printf("The index you entered is greater than the size of the array");
+        return NULL;
+    }
+    if(p->pElemArr[index]==NULL) return NULL;
+    return p->copy_func(p->pElemArr[index]);
 }
 int GetAdptArraySize(PAdptArray p){
-    if(p==NULL) return -1;
-    if(p->pElemArr==NULL) return -1;
+    if(p==NULL){
+        printf("The PAdptArray you entered is null!");
+        return -1;
+    }
+    if(p->pElemArr==NULL){
+        printf("The pElemArr of the PAdptArray you entered is null!");
+        return -1;
+    }
     return p->ArrSize;
 }
 void PrintDB(PAdptArray p){
     if(p==NULL) printf("The PAdptArray you entered is null!");
     if(p->pElemArr==NULL) printf("The pElemArr of the PAdptArray you entered is null!");
     for (int i = 0; i < p->ArrSize; ++i) {
-        if((p->pElemArr)[i] != NULL)
-            p->print_func((p->pElemArr)[i]);
+        if(p->pElemArr[i] != NULL)
+            p->print_func(p->pElemArr[i]);
     }
 }
 
